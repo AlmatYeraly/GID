e95eab774897b1bdcc9266eb070408acba175af2
Utsal Shrestha <ushrestha20@my.whitworth.edu>
2019-05-25 18:11:43 -0700

    needs routes added

--- src/App.js
+++ src/App.js
@@ -8,92 +8,12 @@ class App extends Component {
   constructor(props){
       super(props);
 
-      this.onChangeTodoDescriprtion = this.onChangeTodoDescriprtion.bind(this);
-      this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
-      this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-
       this.state = {
-          todo_description: '',
-          todo_responsible: '',
-          todo_priority: '', 
-          todo_completed: false,
-          redirect: false
       }
-  }
-
-  onChangeTodoDescriprtion(e) {
-      this.setState({
-          todo_description: e.target.value
-      });
-  }
-
-  onChangeTodoResponsible(e) {
-      this.setState({
-          todo_responsible: e.target.value
-      });
-  }
-
-  onChangeTodoPriority(e) {
-      this.setState({
-          todo_priority: e.target.value
-      });
-  }
-
-  onSubmit(e) {
-      e.preventDefault();
-
-      console.log(`Form submitted:`);
-      console.log(`Redirect: ${this.state.redirect}`);
-
-      this.setState({
-       redirect: true
-    });
   }
 
   render() {
-      if (this.state.redirect) {
-        //return (<h1>Test</h1>)
-        return (
-          <BrowserRouter>
-            <Route path="/list" exact component={TodosList} />
-          </BrowserRouter>
-        )
-      }
-      return (
-          <div style={{marginTop: "5%"}}>
-              <div className="container">
-                <h3>GID</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Email: </label>
-                        <input type="email"
-                                className="form-control"
-                                style={{maxWidth: 400}}
-                                value={this.state.todo_description}
-                                onChange={this.onChangeTodoDescriprtion}
-                                />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Password: </label>
-                        <input type="password"
-                                className="form-control"
-                                style={{maxWidth: 400}}
-                                value={this.state.todo_responsible}
-                                onChange={this.onChangeTodoResponsible}
-                                />
-                    </div>
-
-                    <div className="form-group">
-                        <div className="form-group">
-                            <input type="submit" value="Login" className="btn btn-primary" />
-                        </div>
-                    </div>
-                </form>
-              </div>
-          </div>
-      )
+    return(<h1></h1>)
   }
 }
 
--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,119 +1,74 @@
 import React, { Component } from 'react';
 
-export default class CreateTodo extends Component {
+export default class Home extends Component {
     constructor(props){
         super(props);
-
-        this.onChangeTodoDescriprtion = this.onChangeTodoDescriprtion.bind(this);
-        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
-        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
+  
         this.state = {
-            todo_description: '',
-            todo_responsible: '',
-            todo_priority: '', 
-            todo_completed: false
+            redirect: false,
+            email: '',
+            password: ''
         }
     }
-
-    onChangeTodoDescriprtion(e) {
-        this.setState({
-            todo_description: e.target.value
-        });
+  
+    onChange = event => {
+      this.setState({
+        [event.target.id]: event.target.value
+      })
     }
-
-    onChangeTodoResponsible(e) {
-        this.setState({
-            todo_responsible: e.target.value
-        });
-    }
-
-    onChangeTodoPriority(e) {
-        this.setState({
-            todo_priority: e.target.value
-        });
-    }
-
+  
     onSubmit(e) {
         e.preventDefault();
-
+  
         console.log(`Form submitted:`);
-        console.log(`Todo Description: ${this.state.todo_description}`);
-        console.log(`Todo Responsible: ${this.state.todo_responsible}`);
-        console.log(`Todo Priority: ${this.state.todo_priority}`);
-
-        this.state = {
-            todo_description: '',
-            todo_responsible: '',
-            todo_priority: '', 
-            todo_completed: false
-        }
+        console.log(`Redirect: ${this.state.redirect}`);
+  
+        this.setState({
+         redirect: true
+      });
     }
-
+  
     render() {
+        if (this.state.redirect) {
+          //return (<h1>Test</h1>)
+          return (
+            <BrowserRouter>
+              <Route path="/list" exact component={TodosList} />
+            </BrowserRouter>
+          )
+        }
         return (
-            <div style={{marginTop: 10}}>
-                <h3>Create new todo</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Description: </label>
-                        <input type="text"
-                                className="form-control"
-                                value={this.state.todo_description}
-                                onChange={this.onChangeTodoDescriprtion}
-                                />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Responsible: </label>
-                        <input type="text"
-                                className="form-control"
-                                value={this.state.todo_responsible}
-                                onChange={this.onChangeTodoResponsible}
-                                />
-                    </div>
-
-                    <div className="form-group">
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input"
-                                    type="radio"
-                                    name="prorityOptions"
-                                    id="priorityLow"
-                                    value="Low"
-                                    checked={this.state.todo_priority==='Low'}
-                                    onChange={this.onChangeTodoPriority}
-                                    />
-                            <label className="form-check-label">Low</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input"
-                                    type="radio"
-                                    name="prorityOptions"
-                                    id="priorityMedium"
-                                    value="Medium"
-                                    checked={this.state.todo_priority==='Medium'}
-                                    onChange={this.onChangeTodoPriority}
-                                    />
-                            <label className="form-check-label">Medium</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input className="form-check-input"
-                                    type="radio"
-                                    name="prorityOptions"
-                                    id="priorityHigh"
-                                    value="High"
-                                    checked={this.state.todo_priority==='High'}
-                                    onChange={this.onChangeTodoPriority}
-                                    />
-                            <label className="form-check-label">High</label>
-                        </div>
-
-                        <div className="form-group">
-                            <input type="submit" value="Create Todo" className="btn btn-primary" />
-                        </div>
-                    </div>
-                </form>
+            <div style={{marginTop: "5%"}}>
+                <div className="container">
+                  <h3>GID</h3>
+                  <form onSubmit={this.onSubmit}>
+                      <div className="form-group">
+                          <label>Email: </label>
+                          <input type="email"
+                                  className="form-control"
+                                  style={{maxWidth: 400}}
+                                  value={this.state.email}
+                                  onChange={this.handleChange}
+                                  />
+                      </div>
+  
+                      <div className="form-group">
+                          <label>Password: </label>
+                          <input type="password"
+                                  className="form-control"
+                                  style={{maxWidth: 400}}
+                                  value={this.state.password}
+                                  onChange={this.handleChange}
+                                  />
+                      </div>
+  
+                      <div className="form-group">
+                          <div className="form-group">
+                              <input type="submit" value="Login" className="btn btn-primary" />
+                          </div>
+                      </div>
+                  </form>
+                </div>
             </div>
         )
     }
